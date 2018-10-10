6dcb521dfb6d351a88ae91391075c714247373be
mackf <mackf@Macks-Surface-Pro-4>
2018-10-10 15:16:47 -0400

    Finished up anthropometric model

--- AnthropometricModel.m
+++ AnthropometricModel.m
@@ -21,16 +21,54 @@ classdef AnthropometricModel
             map = containers.Map();
             
             % Foot naming global variables 
-            global FootWidth FootLength;
-            % Foot variable declaration
-            map(FootWidth) = height*0.055;
-            map(FootLength) = height*0.152;
+            global footWidth footLength heelToAnkle;
             
+            % Foot variable declaration
+            map(footWidth) = height*0.055;
+            map(footLength) = height*0.152;
+            map(heelToAnkle) = height*0.039;
+            
+            % Leg naming global variables
+            global rightShankLength leftShankLength thighLength;
+            
+            % Leg variable declaration
+            map(rightShankLength) = height*0.246; 
+            map(leftShankLength) = height*0.246;
+            map(thighLength) = height*0.245;
+            
+            % COM naming global variables
+            global pFootCOM pLegCOM pThighCOM pTotalLegCOM ;
+            % Currently not being used: global pFootAndLegCOM pAbdomenCOM pPelvisCOM pAbdomenAndPelvisCOM;
+    
+            % COM variable declarations
+            map(pFootCOM) = footLength*0.5;
+            map(pLegCOM) = rightShankLength*0.433;
+            map(pThighCOM) = thighLength*0.433;
+            map(pTotalLegCOM) = (thighLength + rightShankLength)*0.161;
         end
         
         function map = GetWeight(weight)
             map = containers.Map();
             
+            % Leg and Foot naming global variables
+            global footSegmentWeight legSegmentWeight thighSegmentWeight; 
+            global footAndLegSegmentWeight totalLegSegmentWeight;
+            
+            % Leg and Foot variable declaration
+            map(footSegmentWeight) = weight*0.0145;
+            map(legSegmentWeight) = weight*0.0465;
+            map(thighSegmentWeight) = weight*0.1;
+            map(footAndLegSegmentWeight) = weight*0.061;
+            map(totalLegSegmentWeight) = weight*0.161;
+            
+            % Trunk naming global variables
+            global abdomenSegmentWeight pelvisSegmentWeight;
+            global abdomenAndPelvisSegmentWeight;
+            
+            % Trunk variable declaration
+            map(abdomenSegmentWeight) = weight*0.139;
+            map(pelvisSegmentWeight) = weight*0.142;
+            map(abdomenAndPelvisSegmentWeight) = weight*0.281;
         end
     end    
 end
--- SetAnthropometricNames.m
+++ SetAnthropometricNames.m
@@ -1,19 +1,66 @@
 function SetAnthropometricNames()
     
     % Foot Dimensions Naming
-    global FootWidth FootLength;
-    FootWidth = "Foot Width";
-    FootLength = "Foot Length";
+    global footWidth footLength;
+    footWidth = "Foot Width";
+    footLength = "Foot Length";
     
     % Ankle Dimensions
-    global HeelToAnkle;
-    HeelToAnkle = "Heel To Ankle";
+    global heelToAnkle;
+    heelToAnkle = "Heel To Ankle";
     
     % Leg Dimensions Naming
-    global rightShank leftShank;
-    rightShank = "Right Shank";
-    leftShank = "Left Shank";
+    global rightShankLength leftShankLength;
+    rightShankLength = "Right Shank";
+    leftShankLength = "Left Shank";
     
     % Thigh Dimensions Naming
+    global thighLength;
+    thighLength = "Thigh Length";
+    
+    % Foot Segment Weight Naming
+    global footSegmentWeight;
+    footSegmentWeight = "Foot Segment Weight";
+    
+    % Leg Segment Weight Naming
+    global legSegmentWeight;
+    legSegmentWeight = "Leg Segment Weight";
+    
+    % Thigh Segment Weight Naming
+    global thighSegmentWeight;
+    thighSegmentWeight = "Thigh Segment Weight";
+    
+    % Foot and Leg Segment Weight Naming
+    global footAndLegSegmentWeight;
+    footAndLegSegmentWeight = "Foot and Leg Segment Weight";
+    
+    % Total Leg Segment Weight Naming
+    global totalLegSegmentWeight;
+    totalLegSegmentWeight = "Total Leg Segment Weight";
+    
+    % Abdomen Segment Weight Naming
+    global abdomenSegmentWeight;
+    abdomenSegmentWeight = "Abdomen Segment Weight";
+    
+    % Pelvis Segment Weight Naming
+    global pelvisSegmentWeight;
+    pelvisSegmentWeight = "Pelvis Segment Weight";
+    
+    % Abdomen and Pelvis Segment Weight Naming
+    global abdomenAndPelvisSegmentWeight;
+    abdomenAndPelvisSegmentWeight = "Abdomen and Pelvis Segment Weight";
+    
+    % Centre of Mass Locations from Proximal End of Joint)
+    global pFootCOM pLegCOM pThighCOM pFootAndLegCOM pTotalLegCOM;
+    global pAbdomenCOM pPelvisCOM pAbdomenAndPelvisCOM;
+    
+    pFootCOM = "Foot Center of Mass Location";
+    pLegCOM = "Leg Center of Mass Location";
+    pThighCOM = "Thigh Center of Mass Location";
+    pFootAndLegCOM = "Foot and Leg Center of Mass Location";
+    pTotalLegCOM = "Total Leg Center of Mass Location";
+    pAbdomenCOM = "Abdomen Center of Mass Location";
+    pPelvisCOM = "Pelvis Center of Mass Location";
+    pAbdomenAndPelvisCOM = "Abdomen and Pelvis Center of Mass Location";
     
 end
