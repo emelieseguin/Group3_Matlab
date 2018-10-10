f1202300f347dfae1305356c785f4681cd56857d
nateb <nateb@DESKTOP-R9O10Q7>
2018-10-09 20:19:55 -0400

    Add base classes for anthropometric model to master.

---
+++ SetAnthropometricNames.m
@@ -0,0 +1,19 @@
+function SetAnthropometricNames()
+    
+    % Foot Dimensions Naming
+    global FootWidth FootLength;
+    FootWidth = "Foot Width";
+    FootLength = "Foot Length";
+    
+    % Ankle Dimensions
+    global HeelToAnkle;
+    HeelToAnkle = "Heel To Ankle";
+    
+    % Leg Dimensions Naming
+    global rightShank leftShank;
+    rightShank = "Right Shank";
+    leftShank = "Left Shank";
+    
+    % Thigh Dimensions Naming
+    
+end
