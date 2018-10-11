6b99e431d0afd05013f5b7f2eff4426d682bec22
emelieseguin <43306339+emelieseguin@users.noreply.github.com>
2018-10-11 18:34:17 -0400

    Added Excel columns for Angles

--- GaitDataAngles.m
+++ GaitDataAngles.m
@@ -2,9 +2,21 @@ classdef GaitDataAngles
     properties
         % Angle columns stored from the excel
         time
-        angle1
-        angle2
-        angle3
+        LPelvisAngleX
+        LPelvisAngleY
+        LPelvisAngleZ
+        LHipAngleX
+        LHipAngleY
+        LHipAngleZ
+        LKneeAngleX
+        LKneeAngleY
+        LKneeAngleZ
+        LAnkleAngleX
+        LAnkleAngleY
+        LAnkleAngleZ
+        LFootAngleX       
+        LFootAngleY
+        LFootAngleZ
     end
     methods
         function obj = GaitDataAngles(csvFileName)
@@ -16,10 +28,21 @@ classdef GaitDataAngles
             csvArray=csvread(csvFileName, 1);
             % The second number is the column number
             obj.time = csvArray(:, 1);
-            obj.angle1 = csvArray(:, 2);
-            obj.angle2 = csvArray(:, 3);
-            obj.angle3 = csvArray(:, 4);
-            
+            obj.LPelvisAngleX = csvArray(:, 5);
+            obj.LPelvisAngleY = csvArray(:, 6);
+            obj.LPelvisAngleZ = csvArray(:, 7);
+            obj.LHipAngleX = csvArray(:, 11);
+            obj.LHipAngleY = csvArray(:, 12);
+            obj.LHipAngleZ = csvArray(:, 13);           
+            obj.LKneeAngleX = csvArray(:, 17);           
+            obj.LKneeAngleY = csvArray(:, 18);           
+            obj.LKneeAngleZ = csvArray(:, 19);           
+            obj.LAnkleAngleX = csvArray(:, 23);   
+            obj.LAnkleAngleY = csvArray(:, 24);   
+            obj.LAnkleAngleZ = csvArray(:, 25);   
+            obj.LFootAngleX = csvArray(:, 29);   
+            obj.LFootAngleY = csvArray(:, 30);   
+            obj.LFootAngleZ = csvArray(:, 31);   
             %plot(col1, col2)
         end
     end  
