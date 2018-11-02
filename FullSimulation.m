f5907c86cba172760e57a07432e073b63d8ab28a
Nathan Bowness <nathanbowness@gmail.com>
2018-11-02 17:43:37 -0400

    Fix merge issues resulting for working analysis code. Add main and fullsimulation back after fixing merge conflicts.

---
+++ FullSimulation.m
@@ -0,0 +1,144 @@
+function FullSimulation(fourBarArray, positionArray)   
+    global index;
+    index = 1;
+
+    % Labels on the GUI
+    global hipLabel kneeLabel footLabel;  
+    
+    % Create a UI figure window
+    fig = uifigure;
+    
+    % Create labels on GUI
+    hipLabel = uilabel(fig, 'Position',[460, 240, 100, 20]); 
+    kneeLabel = uilabel(fig, 'Position',[460, 215, 100, 20]);
+    footLabel = uilabel(fig, 'Position',[460, 190, 100, 20]);
+
+    % Create the axis to draw on - positioning is [left bottom width height]
+    ax = uiaxes(fig, 'Position',[50 50 400 400], 'GridLineStyle', 'none');
+    ax.XLim = [-0.5 1];
+    ax.YLim = [-1 0.5];
+    set(ax, 'visible', 'off')    
+    
+    % Create the 3 UI buttons  
+    nextBtn = uibutton(fig,'Position',[440, 30, 100, 22],'Text','Next Position', ...
+        'ButtonPushedFcn', @(nextBtn,event) MoveToNextGaitPositionOnClick(nextBtn,ax, fourBarArray, positionArray));
+    previousBtn = uibutton(fig,'Position',[40, 30, 100, 22],'Text','Previous Position', ...
+        'ButtonPushedFcn', @(previousBtn,event) MoveToPreviousGaitPositionOnClick(previousBtn,ax, fourBarArray, positionArray));
+    
+    simBtn = uibutton(fig,'Position',[460, 150, 75, 20],'Text','Run Sim', ...
+        'ButtonPushedFcn', @(previousBtn,event) RunFullGaitSimOnClick(previousBtn,ax, fourBarArray, positionArray));
+    
+    DrawCurrentLegPosition(ax, positionArray);
+    DrawCurrentFourBarPosition(ax, fourBarArray);
+end
+
+function MoveToNextGaitPositionOnClick(nextBtn, ax, fourBarArray, positionArray)
+        global index;
+        RemoveCurrentLegDrawing(); 
+        RemoveCurrentFourBarDrawing();
+        
+        % If it is the last postion, loop to the first position of the array
+        if(index == length(positionArray))
+            index = 1;
+        else
+            index = index +1;
+        end
+        DrawCurrentLegPosition(ax, positionArray);
+        DrawCurrentFourBarPosition(ax, fourBarArray);
+end
+
+function MoveToPreviousGaitPositionOnClick(previousBtn, ax, fourBarArray, positionArray)
+        global index;
+        RemoveCurrentLegDrawing();
+        RemoveCurrentFourBarDrawing();
+        
+        % If it is the first postion, loop to the end of the array
+        if(index == 1)
+            index = length(positionArray);
+        else
+            index = index -1;
+        end
+        DrawCurrentLegPosition(ax, positionArray);   
+        DrawCurrentFourBarPosition(ax, fourBarArray);
+end
+
+function RunFullGaitSimOnClick(simBtn, ax, fourBarArray, positionArray)
+    global index;
+    RemoveCurrentLegDrawing();
+    RemoveCurrentFourBarDrawing();
+    
+    % Store Index so that it can be set back to the previous frame after
+    % the loop
+    previousIndex = index;
+    index=1;
+    
+    for num = 1:(length(positionArray))
+       DrawCurrentLegPosition(ax, positionArray);
+       DrawCurrentFourBarPosition(ax, fourBarArray);
+       pause(.02);
+       RemoveCurrentLegDrawing();
+       RemoveCurrentFourBarDrawing();
+       index = index + 1;
+    end
+    
+    index = previousIndex;
+    DrawCurrentLegPosition(ax, positionArray);
+    DrawCurrentFourBarPosition(ax, fourBarArray);
+end
+
+function RemoveCurrentLegDrawing()
+    global ThighLine ShankLine AnkleToCalcLine AnkleToMetaLine CalcToToeLine ThighComPoint ShankComPoint FootComPoint;
+    clearpoints(ThighLine);
+    clearpoints(ShankLine);
+    clearpoints(AnkleToCalcLine); 
+    clearpoints(AnkleToMetaLine); 
+    clearpoints(CalcToToeLine);
+    
+    clearpoints(ThighComPoint); 
+    clearpoints(ShankComPoint); 
+    clearpoints(FootComPoint); 
+end
+
+function DrawCurrentLegPosition(ax, positionArray)
+    global index ThighLine ShankLine AnkleToCalcLine AnkleToMetaLine CalcToToeLine ThighComPoint ShankComPoint FootComPoint;
+    global hipLabel kneeLabel footLabel;
+    % Draw the lines to define lower limb
+    ThighLine = animatedline(positionArray(index).ThighPositionX, positionArray(index).ThighPositionY,'Parent', ax, 'Color','r','LineWidth',3);
+    ShankLine = animatedline(positionArray(index).ShankPositionX, positionArray(index).ShankPositionY,'Parent', ax,'Color','r','LineWidth',3);
+    AnkleToCalcLine = animatedline(positionArray(index).AnkleToCalcX, positionArray(index).AnkleToCalcY,'Parent', ax,'Color','r','LineWidth',3);
+    AnkleToMetaLine = animatedline(positionArray(index).AnkleToMetaX, positionArray(index).AnkleToMetaY,'Parent', ax,'Color','r','LineWidth',3);
+    CalcToToeLine = animatedline(positionArray(index).CalcToToeX, positionArray(index).CalcToToeY,'Parent', ax,'Color','r','LineWidth',3);
+    
+    % Draw lines for the Com Pieces
+    ThighComPoint = animatedline(positionArray(index).ThighComXVector, positionArray(index).ThighComYVector,'Parent', ax,'Color','r','LineWidth',3);
+    ShankComPoint = animatedline(positionArray(index).ShankComXVector, positionArray(index).ShankComYVector,'Parent', ax,'Color','r','LineWidth',3);
+    %FootComPoint = animatedline(positionArray(index).FootComXVector, positionArray(index).FootComYVector,'Parent', ax,'Color','r','LineWidth',3);
+    
+    % Add label to the GUI
+    % char(176) is degree
+    hipLabel.Text = ['Hip Angle: ' , num2str(round(positionArray(index).HipAngleZ)), char(176)];
+    kneeLabel.Text = ['Knee Angle: ' , num2str(round(positionArray(index).KneeAngleZ)), char(176)];
+    footLabel.Text = ['Foot Angle: ' , num2str(round(positionArray(index).FootAngleZ)), char(176)];  
+end
+
+function RemoveCurrentFourBarDrawing()
+    global Link1Line Link2Line Link3Line Link4Line;
+    clearpoints(Link1Line);
+    clearpoints(Link2Line);
+    clearpoints(Link3Line); 
+    clearpoints(Link4Line);
+end
+
+function DrawCurrentFourBarPosition(ax, fourBarArray)
+    global index Link1Line Link2Line Link3Line Link4Line;
+    %global intersectPoint;
+    % Draw the lines for the 4 linkages
+    Link1Line = animatedline(fourBarArray(index).Link1X, fourBarArray(index).Link1Y,'Parent', ax,'Color','b','LineWidth',2);
+    Link2Line = animatedline(fourBarArray(index).Link2X, fourBarArray(index).Link2Y,'Parent', ax,'Color','g','LineWidth',2);
+    Link3Line = animatedline(fourBarArray(index).Link3X, fourBarArray(index).Link3Y,'Parent', ax,'Color','b','LineWidth',2);
+    Link4Line = animatedline(fourBarArray(index).Link4X, fourBarArray(index).Link4Y,'Parent', ax,'Color','g','LineWidth',2);
+    
+    % Add label to the GUI
+    %intersectPoint.Text = ['Intersection (X,Y): (' , num2str(round(fourBarArray(index).IntersectPointX)), ...
+    %    ',', num2str(round(fourBarArray(index).IntersectPointY)), ')'] ;
+end
---
+++ Main.m
@@ -0,0 +1,218 @@
+function Main()
+    global rightShankLength;
+    % Names of body parts
+    SetAnthropometricNames(); % Run this to initialize all global naming variables
+    
+    % Build the anthropomtric model
+    personHeight = 1.78; % in m
+    model = AnthropometricModel(personHeight, 50.0);
+
+    patient29AnglesCsvFileName = 'Patient29_Normal_Walking_Angles.csv';
+    patient29ForcesCsvFileName = 'Patient29_Normal_Walking_Forces.csv';
+    patient29CopData_Left = 'Patient29_ForcePlateData_LeftFoot.csv';
+    patient29FootLengthInMm = 295;  % Need to actually find the patientFootLength for correct Moments
+    
+    % Create objects to store the gait information (angles, forces) of patient 29
+    patient29Angles = GaitDataAngles(patient29AnglesCsvFileName);
+    patient29Forces = GaitDataForces(patient29ForcesCsvFileName);
+    
+    % Build the position and 4Bar array from the patientAngle data
+    positionArray = [];
+    fourBarArray = [];
+    kneePointXArray = zeros(1,101);
+    kneePointYArray = zeros(1,101);
+    intersectPointXArray = zeros(1,101);
+    intersectPointYArray = zeros(1,101);
+    plantarFlexionArray = [];
+    springLengthArray = [];
+    dorsiFlexionArray = [];
+    dorsiSpringLengthArray = [];
+    for item = 1:(length(patient29Angles.LFootAngleZ))
+        
+        % Get the angles from the object
+        foot = patient29Angles.LFootAngleZ(item);
+        knee = patient29Angles.LKneeAngleZ(item);
+        hip = patient29Angles.LHipAngleZ(item);
+        ankle = patient29Angles.LAnkleAngleZ(item);
+        
+        % Create the position of the leg, add it to the array
+        position = GaitLegPosition(model, foot, knee, hip);
+        positionArray = [positionArray position];
+        kneePointXArray(item) = position.KneeJointX;
+        kneePointYArray(item) = position.KneeJointY;
+        
+        linkagePosition = FourBarLinkagePosition(personHeight, position.KneeJointX, position.KneeJointY, ...
+            model.dimensionMap(rightShankLength), hip, knee);
+        fourBarArray = [fourBarArray linkagePosition];
+
+        intersectPointXArray(item) = linkagePosition.IntersectPointX;
+        intersectPointYArray(item) = linkagePosition.IntersectPointY;
+    
+        plantarPosition = PlantarFlexionSpringPosition(personHeight, position.AnkleJointX, position.AnkleJointY, ...
+            ankle, foot, position.ShankComXPoint, position.ShankComYPoint);
+        plantarFlexionArray = [plantarFlexionArray plantarPosition];
+        springLengthArray = [springLengthArray plantarPosition.Length];
+        
+        dorsiPosition = DorsiFlexionSpringPosition(personHeight, position.KneeJointX, position.KneeJointY, ...
+            position.AnkleJointX, position.AnkleJointY, position.ThighComXPoint, position.ThighComYPoint, ... 
+            position.FootComXPoint, position.FootComYPoint, position.ShankComXPoint, position.ShankComYPoint, ... 
+            hip, knee, ankle, foot, position.ToeX, position.ToeY);
+        dorsiFlexionArray = [dorsiFlexionArray dorsiPosition];
+        dorsiSpringLengthArray = [dorsiSpringLengthArray dorsiPosition.Length];
+    end   
+    
+    % Calc the linear and angular accelerations 
+    % 1.3 is a dummy time for the gait cycle
+    patient29_HeelStrike = 0;
+    patient29_ToeOff = patient29Forces.ToeOffPercentage;
+    timeForGaitCycle = 1; %0.708;
+    linearAccel = LinearAcceleration(positionArray, timeForGaitCycle);
+    angularAccel = AngularAcceleration(positionArray, timeForGaitCycle);
+    normCopData = NormalizeCopData(patient29CopData_Left, ...
+        patient29_HeelStrike, patient29_ToeOff, patient29FootLengthInMm);
+    
+    % Plot the Linear Velocity and Acceleration
+        %linearAccel.PlotVelocityInterpolationCurves();
+        %linearAccel.PlotAccelerationCurves();
+        
+    % Plot the Angular Velocity and Accelerations
+        %angularAccel.PlotVelocityInterpolationCurves();
+        %angularAccel.PlotAccelerationCurves();
+        
+    % Plot the plantarflexion and dorsiflexion spring
+    %PlotShankSpringLength(springLengthArray);
+    PlotDorsiSpringLength(dorsiSpringLengthArray);
+    % Plot the Intersection of the 4 bar linkage with respect to the knee
+    % joint position
+    %Plot4BarLinkageWRTKneeJoint(kneePointXArray, kneePointYArray, ...
+    %intersectPointXArray, intersectPointYArray);
+    
+    %HipTorsionSpring(patient29Angles.LHipAngleZ);
+    %PlantarSpringCalcs(springLengthArray);
+    %DorsiSpringCalcs();
+    
+    % Run the gait simulation
+    %FourBarLinkageSim(fourBarArray);
+    %GaitSimulation(positionArray);
+    %FullSimulation(fourBarArray, positionArray);
+    %PlantarFlexionSpringSim(plantarFlexionArray);
+    %DorsiFlexionSpringSim(dorsiFlexionArray);
+    FullSimulationPart2(plantarFlexionArray, positionArray, dorsiFlexionArray);
+    
+    inverseDynamics = InverseDynamics(model, positionArray, linearAccel, ...
+        angularAccel, patient29Forces, normCopData);
+    inverseDynamics.PlotMomentGraphs();
+end
+
+%PlotPatientGaitAngles(patient29Angles);
+%PlotPatientGaitForces(patient29Forces);
+
+function PlotShankSpringLength(springLengthArray)
+    %figure 
+    
+    %plot(1:length(springLengthArray), springLengthArray);
+    
+    figure
+            % Plot the plantarflexion cable and spring length graph
+            top = subplot(1,1,1);
+            plot(top, 1:length(springLengthArray), springLengthArray, 'LineWidth',2);
+            hold on
+            grid on
+            title('Plantarflexion Spring and Cable Length vs. Percent Gait Cycle');
+            ylabel('Length (m)')
+            xlabel('Gait Cycle (%)') 
+            set(top, 'LineWidth',1)
+            legend(top, 'Spring and Cable Length')
+            axis(top, [0 length(springLengthArray)  (1)*(max(springLengthArray)+1) (-1)*(min(springLengthArray)-1)])
+
+end
+
+function PlotDorsiSpringLength(dorsiSpringLengthArray)
+    figure
+            % Plot the Hip moment graph
+            top = subplot(1,1,1);
+            plot(top, 1:length(dorsiSpringLengthArray), dorsiSpringLengthArray, 'LineWidth',2);
+            hold on
+            grid on
+            title('Dorsiflexion Spring and Cable Length vs. Percent Gait Cycle');
+            ylabel('Length (m)')
+            xlabel('Gait Cycle (%)') 
+            set(top, 'LineWidth',1)
+            legend(top, 'Spring and Cable Length')
+            axis(top, [0 length(dorsiSpringLengthArray)  (min(dorsiSpringLengthArray)-0.05) (max(dorsiSpringLengthArray)+0.05)])
+end
+
+function Plot4BarLinkageWRTKneeJoint(kneePointXArray, kneePointYArray, ...
+    intersectPointXArray, intersectPointYArray)
+
+    figure
+    top = subplot(2,1,1);
+    plot(top, 1:length(kneePointXArray), kneePointXArray, 'LineWidth',2);
+    hold on
+    grid on
+    plot(top, 1:length(intersectPointXArray), intersectPointXArray, 'LineWidth',2);
+    title('Position of the Knee and Linkage Intersection');
+    ylabel('X Coordinates (m)')
+    xlabel('Gait Cycle (%)') 
+    set(top, 'LineWidth',1)
+    legend(top, 'Knee Center','Linkage Intersection')
+    
+    axis(top, [0 100 -0.2 0.3])
+    
+    bottom = subplot(2,1,2);
+    plot(bottom, 1:length(kneePointYArray), kneePointYArray, 'LineWidth',2);
+    hold on
+    grid on
+    plot(bottom, 1:length(intersectPointYArray), intersectPointYArray, 'LineWidth',2);
+    title('Position of the Knee and Linkage Intersection');
+    xlabel('Gait Cycle (%)') 
+    ylabel('Y Coordinates (m)') 
+    set(bottom, 'LineWidth',1)
+    
+    legend(bottom, 'Knee Center','Linkage Intersection')
+    axis(bottom, [0 100 -0.45 -0.3])
+end
+
+function PlotPatientGaitAngles(patientAngles)
+    figure
+    % Graph number 1
+    top = subplot(4,1,1);
+    plot(top, patientAngles.Time, patientAngles.LHipAngleZ);
+    title(top,'Left Hip Angle Z');
+    %topLeft.FontSize = 14;
+    
+    middle = subplot(4,1,2);
+    plot(middle, patientAngles.Time, patientAngles.LKneeAngleZ);
+    title(middle,'Left Knee Angle Z');
+    grid(middle,'on');
+    
+    middleBottom = subplot(4,1,3);
+    plot(middleBottom, patientAngles.Time, patientAngles.LAnkleAngleZ);
+    title(middleBottom,'Left Ankle Angle Z');
+    grid(middleBottom,'on');
+    
+    bottom = subplot(4,1,4);
+    plot(bottom, patientAngles.Time, patientAngles.LFootAngleZ);
+    title(bottom,'Left Foot Angle Z');
+    grid(bottom,'on');
+end
+
+function PlotPatientGaitForces(patientForces)
+    figure
+    % Graph number 1
+    topLeft = subplot(3,1,1);
+    plot(topLeft, patientForces.Time, patientForces.LGRFX);
+    title(topLeft,'Ground Reaction Force X');
+    %topLeft.FontSize = 14;
+    
+    topModel = subplot(3,1,2);
+    plot(topModel, patientForces.Time, patientForces.LGRFY);
+    title(topModel,'Ground Reaction Force Y');
+    grid(topModel,'on');
+    
+    topModel = subplot(3,1,3);
+    scatter(topModel, patientForces.Time, patientForces.LGRFZ);
+    title(topModel,'Ground Reaction Force Z');
+    grid(topModel,'on');
+end
+
